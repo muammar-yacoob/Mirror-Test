e194c452f76d2ef006969a88176404e27e26e4c2
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2022-12-04 17:33:03 +0000

    Scene using built in NetworkManager

--- Assets/Project/Scripts/MyNetworkManager.cs
+++ Assets/Project/Scripts/MyNetworkManager.cs
@@ -1,34 +1,93 @@
+using System;
 using Mirror;
 using UnityEngine;
 
 namespace BornCore.MirrorDev
 {
-    public class MyNetworkManager : MonoBehaviour
+    public class MyNetworkManager : NetworkBehaviour
     {
-        [SerializeField] GameObject treePrefab;
+        private NetworkConnectionToClient connection;
+        public ConnectionStatus ConnectionStatus { get; private set; }
 
-        public void ClientConnect()
+        private void OnGUI()
         {
-            NetworkClient.RegisterPrefab(treePrefab);
+            if (ConnectionStatus == ConnectionStatus.Connecting)
+            {
+                GUILayout.Label("Connecting...");
+                return;
+            }
+
+            if (ConnectionStatus == ConnectionStatus.Connected)
+            {
+                if (GUILayout.Button("Disconnect"))
+                {
+                    connection.Disconnect();
+                }
+
+                return;
+            }
+
+            if (GUILayout.Button("Host"))
+            {
+                StartServer();
+            }
+
+            if (GUILayout.Button("Client"))
+            {
+                StartClient();
+            }
+        }
+
+
+        public void StartServer()
+        {
+            Application.runInBackground = true;
+            ConnectionStatus = ConnectionStatus.Connecting;
+            NetworkServer.RegisterHandler<ConnectMessage>(OnServerConnect);
+            NetworkServer.RegisterHandler<ReadyMessage>(OnClientReady);
+            NetworkServer
+
+            
+            
+            
+            NetworkServer.Listen(4);
+        }
+
+        private void OnServerConnect(NetworkConnectionToClient connection, ConnectMessage arg2, int arg3)
+        {
+            Debug.Log($"Server: Connected to: {connection.address}");
+            this.connection = connection;
+        }
+        
+        void OnClientReady(NetworkConnectionToClient conn, ReadyMessage msg)
+        {
+            Debug.Log($"Server: Client ready, last log: {conn.lastMessageTime}");
+            NetworkServer.SetClientReady(conn);
+        }
+
+        private void StartClient()
+        {
             NetworkClient.RegisterHandler<ConnectMessage>(OnClientConnect);
             NetworkClient.Connect("localhost");
         }
 
-
-        private void OnClientConnect(string obj)
-        {
-            throw new System.NotImplementedException();
-        }
-
-
         void OnClientConnect(ConnectMessage msg)
         {
-            Debug.Log("Connected to server: ");
+            Debug.Log($"Client: Connected to server");
         }
 
+
+        public struct ConnectMessage : NetworkMessage
+        {
+            public int ID;
+            public Vector3 pos;
+        }
     }
 
-    public struct ConnectMessage : NetworkMessage
+    public enum ConnectionStatus
     {
+        Disconnected,
+        Connecting,
+        Connected
     }
 }
